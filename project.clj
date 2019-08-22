31838a9323c3e6f51f3232e24265235e9f78037c
Lucas Mafra <lucas.mafra95@gmail.com>
2019-08-22 15:47:25 -0300

    More cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject common-clj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Useful stuff for Clojure projects"
+  :url "https://github.com/lucasmafra/common-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
