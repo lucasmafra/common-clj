9d9bed2486d2a1d9ca369b8b6431e0a92bcc7f8a
Lucas Mafra <lucas.mafra95@gmail.com>
2019-09-16 15:46:04 -0300

    Add time/friday? utility

--- src/common_clj/time.clj
+++ src/common_clj/time.clj
@@ -8,3 +8,6 @@
   (let [last-day-of-month (time/adjust date :last-day-of-month)]
     (= last-day-of-month date)))
 
+(s/defn friday? :- s/Bool
+  [date :- LocalDate]
+  (time/friday? date))
--- test/common_clj/time_test.clj
+++ test/common_clj/time_test.clj
@@ -6,6 +6,14 @@
 
 (defn from-string [date] (LocalDate/parse date))
 
+(def monday #date "2019-09-16")
+(def tuesday #date "2019-09-17")
+(def wednesday #date "2019-09-18")
+(def thursday #date "2019-09-19")
+(def friday #date "2019-09-20")
+(def saturday #date "2019-09-21")
+(def sunday #date "2019-09-22")
+
 (s/with-fn-validation
   (tabular
    (fact "last-day-of-month?"
@@ -17,4 +25,16 @@
    "2019-09-30"       true
    "2019-02-28"       true
    "2020-02-28"       false
-   "2020-02-29"       true))
+   "2020-02-29"       true)
+
+  (tabular
+   (fact "friday?"
+     (time/friday? ?date) => ?friday)
+   ?date       ?friday
+   monday      false
+   tuesday     false
+   wednesday   false
+   thursday    false
+   friday      true
+   saturday    false
+   sunday      false)) 
