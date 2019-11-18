17e6e3004e8409a8aad6306677e5995c0c4b382c
Lucas Mafra <lucas.mafra@nubank.com.br>
2019-11-18 15:42:51 -0300

    Remove reference to clojure.test

--- src/common_clj/schemata/http_server.clj
+++ src/common_clj/schemata/http_server.clj
@@ -4,7 +4,7 @@
 (def RouteSettings
   {:path                                s/Str
    :method                              (s/enum :get :put :post :delete :options)
-   :handler                             (s/pred clojure.test/function?)
+   :handler                             s/Any
    (s/optional-key :request-schema)     {s/Any s/Any}
    (s/optional-key :path-params-schema) {s/Any s/Any}
    :response-schema                     {s/Any s/Any}})
