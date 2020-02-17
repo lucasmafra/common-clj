b3e0539c9cf5d2465536c46d3f1eee12e53301f3
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-17 10:50:30 -0300

    clean dev namespace

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,66 +1,8 @@
 (ns dev
   (:require [com.stuartsierra.component :as component]
-            [com.stuartsierra.component.repl :refer [reset set-init stop]]
-            [common-clj.components.config.in-memory-config :as imc]
-            [common-clj.components.http-server.http-server :as hs]
-            [common-clj.schema :as cs]
-            [schema.core :as s]
-            [clojure.core.match :refer [match]]
-            [common-clj.coercion :as coercion]))
-
-(def routes
-  {:route/hello
-   {:path            "/hello"
-    :method          :get
-    :handler         (constantly (hs/ok {:message "Ok"}))
-    :response-schema s/Any}
-
-   :route/demo-path-params
-   {:path               "/demo/path-params/:x"
-    :method             :get
-    :handler            (fn [{{:keys [x]} :path-params} _] (hs/ok {:message (str "x: " x)}))
-    :path-params-schema {:x cs/PosInt}
-    :response-schema    s/Any}
-
-   :route/demo-query-params
-   {:path                "/demo/query"
-    :method              :get
-    :handler             (fn [{{:keys [age name]} :query-params} _]
-                           (hs/ok {:message (str "Age: " age " Name: " name)}))
-    :query-params-schema {:age s/Int :name s/Str}
-    :response-schema     s/Any}
-
-   :route/demo-post
-   {:path            "/demo/post"
-    :method          :post
-    :handler         (fn [{{:keys [age name]} :body} _]
-                       (hs/ok {:new-name (str name "Bob")}))
-    :request-schema  {:date cs/LocalDate :age cs/PosInt :email cs/Email}
-    :response-schema {:new-name s/Str}}
-
-   :route/demo-500
-   {:path "/demo/internal-server-error"
-    :method :get
-    :handler (fn [_ _] (hs/ok {:result (/ 1 0)}))
-    :response-schema s/Any}})
-
-(def config
-  {:app-name :hello-world
-   :http-port 9000})
-
-(def coercers
-  (merge
-   hs/default-coercers
-   {cs/LocalDate coercion/local-date-matcher}))
-
-(def overrides
-  {:override-coercers coercers})
+            [com.stuartsierra.component.repl :refer [set-init]]))
 
 (def system
-  (component/system-map
-   :config (imc/new-config config :dev)
-   :http-server (component/using
-                 (hs/new-http-server routes overrides)
-                 [:config])))
+  (component/system-map))
 
 (set-init (constantly system))
