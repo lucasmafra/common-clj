af2bc714707cb8f9fd951041247d4ed35c33a372
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-17 18:16:40 -0300

    fix request-arrived

--- src/common_clj/state_flow_helpers/http.clj
+++ src/common_clj/state_flow_helpers/http.clj
@@ -3,10 +3,16 @@
             [io.pedestal.test :as test]
             [state-flow.state :as state]))
 
+(defn- prettify-body [body]
+  (try
+    (json/string->json body)
+    (catch Exception e
+      body)))
+
 (defn request-arrived! [method path]
   (state/gets
    (fn [world]
      (let [service-fn (-> world :system :http-server :service :io.pedestal.http/service-fn)
            {:keys [status body]} (test/response-for service-fn method path)]
        {:status status
-        :body   (json/string->json body)}))))
+        :body   (json/string->json (prettify-body body))}))))
