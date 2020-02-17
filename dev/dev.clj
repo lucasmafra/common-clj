762bc85de78372071f311fc016539f5399478439
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-17 10:52:01 -0300

    fixed untracked files

--- dev/dev.clj
+++
@@ -1,8 +0,0 @@
-(ns dev
-  (:require [com.stuartsierra.component :as component]
-            [com.stuartsierra.component.repl :refer [set-init]]))
-
-(def system
-  (component/system-map))
-
-(set-init (constantly system))
