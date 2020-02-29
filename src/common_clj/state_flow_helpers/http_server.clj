b283cfa135b3973fac12317f2ec7b52e916ac59c
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-29 18:12:14 -0300

    improve http_server helpers

--- src/common_clj/state_flow_helpers/http_server.clj
+++ src/common_clj/state_flow_helpers/http_server.clj
@@ -9,11 +9,18 @@
     (catch Exception e
       body)))
 
-(defn request-arrived! [method path]
+(defn request-arrived! [method path & options]
   (state/gets
    (fn [world]
      (let [service-fn (or (-> world :system :http-server :service :io.pedestal.http/service-fn)
                           (throw (AssertionError. "No http server found in the system")))
-           {:keys [status body]} (test/response-for service-fn method path)]
+           {:keys [status body]} (apply test/response-for service-fn method path options)]
        {:status status
         :body   (prettify-body body)}))))
+
+(def GET (partial request-arrived! :get))
+(def POST (partial request-arrived! :post))
+(def PUT (partial request-arrived! :put))
+(def DELETE (partial request-arrived! :delete))
+(def OPTIONS (partial request-arrived! :options))
+(def HEAD (partial request-arrived! :head))
