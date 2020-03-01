2aa52f85ca7e696c21425497b3a954bd66c5b8d1
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-01 10:10:53 -0300

    fix linter

--- test/common_clj/coercion_test.clj
+++ test/common_clj/coercion_test.clj
@@ -1,17 +1,1 @@
 (ns common-clj.coercion-test)
-
-#_(s/with-fn-validation
-    (facts "coerce"
-           (fact "BigDecimal"
-                 (coercion/coerce java.math.BigDecimal 20) => 20M)
-
-           (fact "LocalDate"
-                 (coercion/coerce java.time.LocalDate "2019-08-22") => #local-date "2019-08-22")
-
-           (fact "LocalDateTime"
-                 (coercion/coerce java.time.LocalDateTime "2019-08-22T12:52:37")
-                 => #local-date-time "2019-08-22T12:52:37")
-
-           (fact "when it can't coerce it throws schema error"
-                 (coercion/coerce java.math.BigDecimal "twenty")
-                 => (throws ExceptionInfo coercion-error?))))
