0306a7e7144a3eea79d028935da96a00044ce0b5
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-07 10:25:57 -0300

    Improve enum coercion to handle underscore cased values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Change Log
+### Changed [1.3.0]
+- [coercion] Improve enum coercer to handle snake-cased values
+
 ### Changed [1.1.1]
 - [http-server] Add CORS [*] when in dev mode
 - [http-server] Add service-map override
--- test/common_clj/coercion_test.clj
+++ test/common_clj/coercion_test.clj
@@ -1,1 +1,24 @@
-(ns common-clj.coercion-test)
+(ns common-clj.coercion-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [common-clj.coercion :as nut]
+            [common-clj.schema.core :as cs]
+            [schema.core :as s]))
+
+(deftest local-date
+  (testing "coerces"
+    (is (= #local-date "2012-02-12"
+           (nut/coerce cs/LocalDate "2012-02-12")))))
+
+(deftest local-date-time
+  (testing "coerces"
+    (is (= #local-date-time "2012-02-12T20:00:00"
+           (nut/coerce cs/LocalDateTime "2012-02-12T20:00:00")))))
+
+(deftest enum
+  (testing "coerces"
+    (is (= :a
+           (nut/coerce (s/enum :a :b) "a"))))
+
+  (testing "accepts uppercase and underscore"
+    (is (= :value-a
+           (nut/coerce (s/enum :value-a :value-b) :VALUE_A)))))
